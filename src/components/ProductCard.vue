f00612c180beb6f05ad3a32f3d439d54a7e864e8
TheanRogerio <thean.rogerio@gmail.com>
2026-03-18 19:50:58 -0300

    Implement product component

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card>
+    <v-img :src="product.image" height="300px" />
+
+    <v-card-title>
+      {{ product.name }}
+    </v-card-title>
+
+    <v-card-subtitle>
+      R$ {{ product.price }}
+    </v-card-subtitle>
+
+    <v-card-text>
+      {{ product.description }}
+    </v-card-text>
+  </v-card>
+</template>
+
+<script setup>
+  defineProps({
+    product: Object
+  })
+</script>
